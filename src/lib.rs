58cb3ed6a172fe83acb5671bc0cec914e90b692e
Florent Valette <florent.valette@ledger.fr>
2024-11-15 14:21:08 +0100

    lib.rs: implement IntoIterator trait for DotConfig
    And thus do not expose implementation detail (i.e. HashMap).
    Also convert hex to int in build.rs instead of macro_rules
    as `strip_prefix` is not const (i.e. constant expression).

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@
 use std::collections::HashMap;
 use std::env;
 
-pub struct DotConfig<'a>(pub HashMap<&'a str, &'a str>);
+pub struct DotConfig<'a>(HashMap<&'a str, &'a str>);
 
 impl<'a> From<HashMap<&'a str, &'a str>> for DotConfig<'a> {
     fn from(map: HashMap<&'a str, &'a str>) -> Self {
@@ -29,6 +29,31 @@ impl<'a> From<&'a str> for DotConfig<'a> {
     }
 }
 
+impl<'a> IntoIterator for DotConfig<'a> {
+    type Item = <HashMap<&'a str, &'a str> as IntoIterator>::Item;
+    type IntoIter = <HashMap<&'a str, &'a str> as IntoIterator>::IntoIter;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
+
+fn add_cfg<'a>(name: &'a str) {
+    println!("cargo::rustc-check-cfg=cfg({name},values(none()))");
+    println!("cargo::rustc-cfg={name}");
+}
+
+fn add_option_env<'a>(name: &'a str, value: &'a str) {
+    match value.strip_prefix("0x") {
+        Some(stripped) => {
+            let numerical_value = u64::from_str_radix(stripped, 16).unwrap();
+            println!("cargo::rustc-env={name}={numerical_value}");
+            println!("cargo::rustc-env={name}_STR_HEX={value}");
+        },
+        None => println!("cargo::rustc-env={name}={value}"),
+    }
+}
+
 pub fn import_dotconfig_from_script() {
     let dotconfig_filename = &env::var("config").unwrap_or(".config".to_string());
     let data = std::fs::read_to_string(dotconfig_filename).unwrap();
@@ -37,18 +62,14 @@ pub fn import_dotconfig_from_script() {
     println!("cargo::rerun-if-env-changed=config");
     println!("cargo::rerun-if-changed={dotconfig_filename}");
 
-    // XXX:
-    // dotconfig does not extend HashMap but it's a tuple w/ one member
-    // Thus, one needs to access first tuple element
-    // TODO: Is it possible to extend type (or at least, implements iter trait) ?
-    for (key, value) in dotconfig.0.into_iter() {
-        if value == "y" {
-            println!("cargo::rustc-check-cfg=cfg({key},values(none()))");
-            println!("cargo::rustc-cfg={key}");
-        } else {
-            println!("cargo::rustc-check-cfg=cfg({key},values(\"{value}\"))");
-            println!("cargo::rustc-cfg={key}=\"{value}\"");
-            println!("cargo::rustc-env={key}={value}");
+    for (key, value) in dotconfig.into_iter() {
+        match value {
+            "n" | "m"   => (), // skip boolean (or tristate) set to n (or m)
+            "y"         => add_cfg(key),
+            _           => {
+                add_cfg(key);
+                add_option_env(key, value)
+            },
         }
     }
 }
